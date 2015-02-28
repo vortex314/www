b3e5d7a425305c2277824f91596dacb5cdf3d6f9
Lieven Merckx <lieven.merckx@gmail.com>
2015-02-28 02:04:13 +0100

    initial

---
+++ mq.js
@@ -0,0 +1,204 @@
+var mqttApp = angular.module('mqttApp', []);
+
+mqttCtrl = mqttApp.controller('mqttCtrl', function ($scope) {
+    var client;// = new Messaging.Client("pcacer", 80, "mosqOtti");
+    $scope.updates = 0;
+    $scope.host = "pcacer";
+    $scope.port = 80;
+    $scope.subscription = "#"
+    $scope.log = "";
+    $scope.topics = {};
+    $scope.connect = function () {
+        client = new Messaging.Client($scope.host, $scope.port, "mqtt" + Math.random());
+        client.startTrace();
+        client.onMessageArrived = $scope.messageArrived;
+        client.onMessageDelivered = $scope.messageDelivered;
+        client.connect({
+            onSuccess: function (object) {
+                $scope.log = "Connection succeeded" + object;
+                $scope.subscribe();
+            },
+            onFailure: function (err) {
+                $scope.log = 'Connect fail. ErrorCode:' + err.errorCode + ', ErrorMsg: ' + err.errorMessage;
+            }
+        });
+    };
+    $scope.getTopic = function (str) {
+        if (typeof $scope.topics[str] === "undefined") return "UNDEF"
+        else
+            return $scope.topics[str];
+    };
+    $scope.setTopic = function (topic, value) {
+    };
+    $scope.disconnect = function () {
+        client.disconnect();
+    }
+    $scope.subscribe = function () {
+        client.subscribe("#", {
+            qos: 2,
+            onSuccess: function () {
+            },
+            onFailure: function () {
+                alert("subscribe failed ! ");
+            }
+        })
+    };
+
+    $scope.messageArrived = function (message) {
+        $scope.updates++;
+        isMessageReceived = true;
+        try {
+            console.log("messageArrived.", 'topic:', message.destinationName, ' ;content:', message.payloadString);
+            $scope.log = message.destinationName + ' : ' + message.payloadString;
+            topic = message.destinationName;
+
+            if (topic.lastIndexOf(".META") == (topic.length - 5)) { // is it META data ? 
+                topic = topic.substr(0, topic.length - 5);
+                if (typeof $scope.topics[topic] === "undefined") {
+                    $scope.topics[topic] = {"count": 0};
+                }
+                $scope.topics[topic].meta = JSON.parse(message.payloadString);
+            }
+            else { // is it just value update ? 
+                if (typeof $scope.topics[topic] === "undefined") {
+                    $scope.topics[topic] = {"count": 0};
+                }
+                $scope.topics[topic].value = message.payloadString;
+                $scope[topic] = message.payloadString;
+                $scope.topics[topic].count += 1;
+                $scope.topics[topic].name = topic;
+            }
+            ;
+        } catch (err) {
+            console.log("Exception : " + err + " : " + err.stack);
+        }
+        ;
+        $scope.$apply();
+    };
+    $scope.messageDelivered = function (response) {
+        console.log("messageDelivered. topic:%s, duplicate:%s, payloadString:%s,qos:%s,retained:%s", response.destinationName, response.duplicate, response.payloadString, response.qos, response.retained);
+        isMessageDelivered = true;
+        //reponse.invocationContext.onMessageArrived = null;
+    };
+});
+
+mqttCtrl.directive('deltaTime', function () {
+    return {
+        restrict: 'C',
+        template: '<p class="bg-primary">{{topicObject.name}}</p><h1 class="bg-primary">{{strTime()}}</h1>',
+        scope: {
+            topicObject: '=topic'
+        },
+        controller: function ($scope) {
+            $scope.strTime = function () {
+                if ($scope.topicObject) {
+                    var totalSec = $scope.topicObject.value / 1000;
+                    var days = parseInt(totalSec / (3600*24));
+                    var hours = parseInt(totalSec / 3600) % 24;
+                    var minutes = parseInt(totalSec / 60) % 60;
+                    var seconds = Math.floor(totalSec % 60);
+
+                    var result = days + " days "+(hours < 10 ? "0" + hours : hours) + ":" + (minutes < 10 ? "0" + minutes : minutes) + ":" + (seconds < 10 ? "0" + seconds : seconds + "");
+                    return result;
+                }
+                else
+                    return "hh:mm:ss"
+            }
+        },
+        link: function (scope, element, attrs) {
+            scope.topic3 = scope.$parent.getTopic(attrs.topic);
+        }
+    }
+});
+
+mqttCtrl.directive('absTime', function () {
+    return {
+        restrict: 'C',
+        template: '<div><h4>Topic --<kbd>{{strTime()}}</kbd></h4></div>',
+        scope: {
+            topicObject: '=topic'
+        },
+        controller: function ($scope) {
+            $scope.strTime = function () {
+                if ($scope.topicObject) {
+                    var date = new Date(parseInt($scope.topicObject.value));
+                    var result = "" + date.toISOString();
+                    return result;
+                }
+                else
+                    return "yy/mm/dd hh:mm:ss"
+            }
+        },
+        link: function (scope, element, attrs) {
+            scope.topic3 = scope.$parent.getTopic(attrs.topic);
+        }
+    }
+});
+
+mqttCtrl.directive('lmrGauge', function () {
+    return {
+        restrict: 'C',
+        template: '<div style="width:400; height:300">{{drawChart()}}</div>',
+//        templateUrl: 'lmr-gauge.html',
+        scope: {
+            topicObject: '=topic'
+        },
+        controller: function ($scope) {
+        google.load("visualization", "1.0", {'packages': ["corechart","gauge"]});
+
+            $scope.intValue = function () {
+                            if ($scope.topicObject) {
+                                var valueFloat = parseFloat($scope.topicObject.value);
+                                var valueInt = Math.floor(valueFloat*10);
+                                $scope.data.setValue(0, 1, valueInt);
+                                if ( $scope.options.min > valueInt ) $scope.options.min=valueInt;
+                                if ( $scope.options.max < valueInt ) $scope.options.max=valueInt;
+                                $scope.chart.draw($scope.data, $scope.options);
+                                return  valueInt;
+                            }
+                            else
+                                return 0;
+                        };
+        },
+        link: function (scope, element, attrs) {
+            google.load("visualization", "1.0", {'packages': ["corechart","gauge"]});
+            scope.attrs = attrs;
+            scope.drawChart = function(){
+                if ( scope.data ) {
+                    scope.data.setValue(0, 1, scope.intValue());
+                    scope.chart.draw(scope.data, scope.options);
+                } else {
+                scope.data = google.visualization.arrayToDataTable([
+                    ['Label', 'Value'],
+                    ['Temp', 50]
+                    ]);
+                scope.options = {
+                    width: 400, height: 120,
+                     minorTicks: 1,max:190,min:191
+                    };
+                scope.chart = new google.visualization.Gauge(element[0]);
+                }
+
+            };
+  //          google.setOnLoadCallback(scope.drawChart);
+
+            element.on('load',scope.drawChart);
+        }
+    }
+});
+
+mqttCtrl.directive('lmrG', function () {
+ return {
+        restrict: 'C',
+        template: '<div style="width:400; height:300"></div>',
+//        templateUrl: 'lmr-gauge.html',
+        scope: {
+            topicObject: '=topic'
+        },
+    link : function (scope, element, attrs) {
+        scope.$watch(attrs.topic, function () {
+            console.log(attrs.topic);
+        });
+    }
+    }
+});
